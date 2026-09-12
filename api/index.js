da3661563e4c8878a68bc4ff210cfb9b500bb4bb
TAWFEEQ-M <250071601122@crescent.education>
2026-09-12 18:03:59 +0530

    Configure Netlify API deployment

--- api/index.js
+++ api/index.js
@@ -1,19 +1,8 @@
+import serverless from "serverless-http";
 import app from "../server/index.js";
-import connectDB from "../server/config/db.js";
 
-let dbConnection;
+const handler = serverless(app);
 
-export default async function handler(req, res) {
-  try {
-    if (!dbConnection) dbConnection = connectDB();
-    await dbConnection;
-
-    return app(req, res);
-  } catch (error) {
-    dbConnection = undefined;
-    console.error("API error:", error);
-    return res.status(500).json({
-      message: "Server error",
-    });
-  }
+export default async function handlerFunction(req, res) {
+  return handler(req, res);
 }
